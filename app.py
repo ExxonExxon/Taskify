2d57d35dd4c7ffa163720bb45aaacc56b4915413
ExxonExxon <132812480+ExxonExxon@users.noreply.github.com>
2023-08-20 21:17:20 +1000

    Update app.py

--- app.py
+++ app.py
@@ -714,4 +714,4 @@ def get_tasks():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
